57372ce9f83e2f96849fd48e6af792312c8f53ae
LazaroPerviu <lazaropervi@gmail.com>
2023-10-09 16:31:54 -0300

    asd

---
+++ Acciones/js.js
@@ -0,0 +1,32 @@
+const menu= document.getElementById("menu_icono");
+const cerrar_ventana= document.getElementById("menu_url");
+const galeria = document.getElementsByClassName("galeria")[0];
+const gal = document.getElementById("gal");
+let flag = 0
+
+menu.addEventListener('click', ()=>{
+    cerrar_ventana.classList.toggle("Rocio");
+})
+function expand(id) {
+  const image = document.getElementById(id);
+  var galarray = galeria.children;
+  for (let index = 0; index < galarray.length; index++) {
+    galeria.children[index].classList.remove("expand");
+  }
+  if (image.classList.contains("expand")) {
+    console.log(image.classList);
+  } else {
+    image.classList.toggle("expand");
+  }
+gal.addEventListener("mousedown", ()=>{
+  image.classList.remove("expand")
+
+})
+}
+gal.addEventListener("mouseover", ()=>{
+  console.log("holaaa")
+  if(flag== 0){
+    flag =1
+    alert("Prueba a cliquear las imagenes!")
+  }
+})
